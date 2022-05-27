4dcd0e7fee210adf65bc93622d90b301a709ce66
Mochi <mochitreets0304@gmail.com>
2022-05-27 16:51:54 +0800

    JS部分完成

--- js/all.js
+++ js/all.js
@@ -3,4 +3,24 @@ $(document).ready(function(){
     $('.container').on('click','.totop',function(e){
         $('html,body').animate({scrollTop:0},800);
     })
+
+    $('.qa').on('click','ul .more_li .more',function(e){
+        e.preventDefault();
+        $(this).parent().find('ul').slideToggle(1000);
+    })
+
+      
+    const swiper = new Swiper('.swiper', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+      
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+      
+      });
+
+      
 })
